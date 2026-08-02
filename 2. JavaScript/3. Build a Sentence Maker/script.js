a834f3c09305ad4de2159bf7e838d43dd64b95de
Dr Silly Pepper <jaloveikhilt@gmail.com>
2026-08-02 14:01:24 +0300

    Doing sentence maker
    idk

---
+++ 2. JavaScript/3. Build a Sentence Maker/script.js
@@ -0,0 +1,22 @@
+let adjective = "drunk";
+let noun = "Kekkonen";
+let verb = "Fap";
+let place = "The land of the goons";
+let adjective2 = "stupid";
+let noun2 = "God";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: "+firstStory);
+
+adjective = "more drunken";
+noun = "santa claus";
+verb = "racing";
+place = "Stockholm"
+adjective2 = "Blyatifull";
+noun2 = "Sadness";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+
+console.log("Second story: "+secondStory);
