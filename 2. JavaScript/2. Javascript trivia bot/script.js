8ff4ed6d22d8abe901dcb7a381232785ed25c5cf
Dr Silly Pepper <jaloveikhilt@gmail.com>
2026-08-02 13:52:01 +0300

    Doing javascript trivia bot
    idk

---
+++ 2. JavaScript/2. Javascript trivia bot/script.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Kekkonen";
+let botLocation = "Kuuba";
+let favoriteLanguage = "Huutikset";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = "Olen homo lmao "+favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "Ehkä en sittenkään ole homo "+favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "vittu kun olen vammainen "+favoriteLanguage;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
+
+
