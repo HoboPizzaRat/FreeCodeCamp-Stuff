77626549058c8e2c7637d87eae3667180ab78d52
Dr Silly Pepper <jaloveikhilt@gmail.com>
2026-08-28 19:04:51 +0300

    idk
    idk

---
+++ 3. Front end Development and Libraries/1. React Fundamentals/2. Build a Reusable footer/script.js
@@ -0,0 +1,22 @@
+export const Footer = () => {
+
+  return (
+    <footer>
+      <ul>
+        <li>kekkonen</li>
+        <li>kekkonen</li>
+      </ul>
+      <ul>
+        <li>kekkonen</li>
+        <li>kekkonen</li>
+      </ul>
+      <p>Copyright© No pun Intended</p>
+      <ul className="icons">
+        <li><a href="#">🛠️</a></li>
+        <li><a href="#">🖥️</a></li>
+        <li><a href="#">🧩</a></li>
+        <li><a href="#">🧠</a></li>
+      </ul>
+    </footer>
+  )
+}
